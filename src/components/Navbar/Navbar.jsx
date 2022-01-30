f3eddd423c0f1349a08fc781324b0f6e823ab1a9
Marian-Janolek <m.janolek@gmail.com>
2022-01-30 15:37:30 +0100

    add navbar and header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
-
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="app-navbar">
+      <div className="app-navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className="app-navbar-links">
+        <li className="p-opensans">
+          <a href="#home">home</a>
+        </li>
+        <li className="p-opensans">
+          <a href="#about">about</a>
+        </li>
+        <li className="p-opensans">
+          <a href="#menu">menu</a>
+        </li>
+        <li className="p-opensans">
+          <a href="#awards">awards</a>
+        </li>
+        <li className="p-opensans">
+          <a href="#contact">contact</a>
+        </li>
+      </ul>
+      <div className="app-navbar-login">
+        <a href="#login" className="p-opensans">
+          log in / register
+        </a>
+        <div />
+        <a href="/" className="p-opensans">
+          book a table
+        </a>
+      </div>
+      <div className="app-navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app-navbar-smallscreen-overlay flex-center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay-close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app-navbar-smallscreen-links">
+              <li className="p-opensans">
+                <a href="#home">home</a>
+              </li>
+              <li className="p-opensans">
+                <a href="#about">about</a>
+              </li>
+              <li className="p-opensans">
+                <a href="#menu">menu</a>
+              </li>
+              <li className="p-opensans">
+                <a href="#awards">awards</a>
+              </li>
+              <li className="p-opensans">
+                <a href="#contact">contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
 import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app-header app-wrapper section-padding" id="home">
+    <div className="app-wrapper-info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app-header-h1">The Key To Find Dining</h1>
+      <p className="p-opensans" style={{ margin: '2rem 0rem' }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </p>
+      <button type="button" className="custom-button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app-wrapper-img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
