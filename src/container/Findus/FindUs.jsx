fe8b638e3d3e6e7d9d6df5b799c848acd8b2856d
Marian-Janolek <m.janolek@gmail.com>
2022-01-30 19:23:28 +0100

    add gallery and find us

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import { images, data } from '../../constants';
+import { SubHeading } from '../../components';
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app-bg app-wrapper section-padding" id="contact">
+    <div className="app-wrapper-info">
+      <SubHeading title="contact" />
+      <h1 className="headtext-cormorant" style={{ marginBottom: '3rem' }}>
+        find us
+      </h1>
+      <div className="app-wrapper-content">
+        <p className="p-opensans">
+          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+        </p>
+        <p
+          className="p-cormorant"
+          style={{ color: '#dcca87', margin: '2rem 0' }}
+        >
+          Opening Hours
+        </p>
+        <p className="p-opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="p-opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button className="custom-button" style={{ marginTop: '2rem' }}>
+        visit us{' '}
+      </button>
+    </div>
+    <div className="app-wrapper-img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs';
+import { useRef } from 'react';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const galleryImages = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
+
+const Gallery = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app-gallery flex-center">
+      <div className="app-gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext-cormorant">Photo Gallery</h1>
+        <p className="opensans" style={{ color: '#aaa', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom-button">
+          view more
+        </button>
+      </div>
+      <div className="app-gallery-images">
+        <div className="app-gallery-images-container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div
+              className="app-gallery-images-card flex-center"
+              key={`gallery-image-${index + 1}`}
+            >
+              <img src={image} alt="gallery image" />
+              <BsInstagram className="gallery-image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app-gallery-images-arrows">
+          <BsArrowLeftShort
+            className="gallery-arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery-arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
