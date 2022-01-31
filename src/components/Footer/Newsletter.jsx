fd6e7c1c058f6dc533d7073127f0caa66b6e7171
Marian-Janolek <m.janolek@gmail.com>
2022-01-31 10:54:18 +0100

    add footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app-newsletter">
+    <div className="app-newsletter-heading">
+      <SubHeading title="newsletter" />
+      <h1 className="headtext-cormorant">subscribe to our newsletter</h1>
+      <p className="p-opensans">and never missl atest updates!</p>
+    </div>
+    <div className="app-newsletter-input flex-center">
+      <input type="email" placeholder="Enter your email address" />
+      <button className="custom-button">subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app-footer section-padding">
+    <FooterOverlay />
+    <Newsletter />
+    <div className="app-footer-links">
+      <div className="app-footer-links-contact">
+        <h1 className="app-footer-headtext">contact us</h1>
+        <p className="p-opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p-opensans">+1 212-344-1230</p>
+        <p className="p-opensans">+1 212-555-1230</p>
+      </div>
+      <div className="app-footer-links-logo">
+        <img src={images.gericht} alt="footer-logo" />
+        <p className="p-opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others.”
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon-img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app-footer-links-icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app-footer-links-work">
+        <h1 className="app-footer-headtext">working hours</h1>
+        <p className="p-opensans"></p>
+        <p className="p-opensans">Monday-Friday:</p>
+        <p className="p-opensans">08:00 am -12:00 am</p>
+        <p className="p-opensans">Saturday-Sunday:</p>
+        <p className="p-opensans"> 07:00am -11:00 pm</p>
+      </div>
+    </div>
+    <div className="footer-copyright">
+      <p className="p-opensans">
+        <span>{new Date().getFullYear().toString()}</span> Gericht, All Rights
+        reserved{' '}
+      </p>
+    </div>
   </div>
 );
 
